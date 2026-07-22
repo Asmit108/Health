d4915320664ef980eea83b3f2359907d03526903
asmit <asmitmbi108@gmail.com>
2026-07-22 22:44:41 +0530

    added test to check value of db url

--- src/test/java/com/health/check/CheckApplicationTests.java
+++ src/test/java/com/health/check/CheckApplicationTests.java
@@ -1,10 +1,23 @@
 package com.health.check;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class CheckApplicationTests {
+
+	@Autowired
+    Environment env;
+
+	@Test
+	void printConfig() {
+		System.out.println("Profiles = " + Arrays.toString(env.getActiveProfiles()));
+		System.out.println("Datasource = " + env.getProperty("spring.datasource.url"));
+	}
 
 	@Test
 	void contextLoads() {
