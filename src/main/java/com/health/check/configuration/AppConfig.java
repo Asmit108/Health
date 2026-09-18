3bd0cdcbd5844cdb45c223e8c63514b2bf5c0589
asmit <asmitmbi108@gmail.com>
2026-09-18 22:28:32 +0530

    updated frontend url

--- src/main/java/com/health/check/configuration/AppConfig.java
+++ src/main/java/com/health/check/configuration/AppConfig.java
@@ -108,7 +108,7 @@ public class AppConfig {
             CorsConfiguration cfg = new CorsConfiguration();
 
             // Allow requests from frontend (localhost:3000)
-            cfg.setAllowedOrigins(List.of("http://13.204.66.133:3000")); // no trailing slash
+            cfg.setAllowedOrigins(List.of("https://13.204.66.133:3000")); // no trailing slash
 
             // Allow standard HTTP methods
             cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
